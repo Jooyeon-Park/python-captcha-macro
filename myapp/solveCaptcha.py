3729b10cef2dd0364cfe4f553cf7f259a9a748da
Jooyeon-Park <jooyeonpark0131@gmail.com>
2024-02-06 17:10:34 -0700

    2captcha update

--- myapp/solveCaptcha.py
+++ myapp/solveCaptcha.py
@@ -1,38 +1,27 @@
 import sys
 import os
 from dotenv import load_dotenv
-import capsolver
-import base64
-from pathlib import Path
+from twocaptcha import TwoCaptcha
 
 def solveCaptcha():
     load_dotenv()
 
-    api_key = os.getenv('APIKEY_CapSolver')
+    api_key = os.getenv('APIKEY_2CAPTCHA')
 
     if not api_key:
         sys.exit('APIKEY_2CAPTCHA is not set in the .env file.')
-    # script_directory = os.path.dirname(os.path.abspath(__file__))
-    # captcha_file = os.path.join(script_directory, 'screenshot.png')
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    captcha_file = os.path.join(script_directory, '..', 'screenshot.png')
 
-    # # Check if 'test.png' is a valid file
-    # if not os.path.isfile(captcha_file):
-    #     print(f"'{captcha_file}' is not a valid file.")
-
-    with open("screenshot.png", "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+    # Check if 'test.png' is a valid file
+    if not os.path.isfile(captcha_file):
+        print(f"'{captcha_file}' is not a valid file.")
 
     try:
-        capsolver.api_key = api_key
-        solution = capsolver.solve({
-                "type": "ImageToTextTask",
-                "module": "common",
-                "body": encoded_string
-        })
-        print(solution)
-        return str(solution["text"])
-
+        solver = TwoCaptcha(api_key)
+        result = solver.normal(captcha_file,caseSensitive=1)
+        return str(result["code"])
+    
     except Exception as e:
         print(e)
         return ""
-        #sys.exit(e)
